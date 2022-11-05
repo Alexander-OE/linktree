69a1acd9e8f9a4ed0bc3c178e6978d6aabdf8a89
Egbe Alexander <alexsucceeds@gmail.com>
2022-11-05 23:56:13 +0100

    Refactored mobile view

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,6 @@
 import "./Contact.css";
-
+import Zuri from "./img/zuri.png";
+import ing from "./img/ing.png";
 const Contact = () => {
   return <>
     <form action="">
@@ -10,17 +11,17 @@ const Contact = () => {
         <div className="text">
             <div id="first__name">
                 <h4>First name</h4>
-                <input type="text" className="first__input" placeholder="Enter your first name"/>
+                <input type="text" className="first__input" placeholder="Enter your first name" required/>
             </div>
             <div id="last__name">
                 <h4>Last name</h4>
-                <input type="text" className="last__input" placeholder="Enter your last name"/>
+                <input type="text" className="last__input" placeholder="Enter your last name" required/>
             </div>
         </div>
 
         <div id="email">
             <h4>Email</h4>
-            <input type="text" className="email__input" placeholder="yourname@email.com"/>
+            <input type="text" className="email__input" placeholder="yourname@email.com" required/>
         </div>
 
         <div className="group">
@@ -33,6 +34,11 @@ const Contact = () => {
             <h5>You agree to providing your data to Alexander who may contact you.</h5>
         </div>
         <button id="btn__submit">Send message</button>
+        <div className="footer">
+      <img src={Zuri} alt="zuri__img" />
+      <h3>HNG Internship 9 Frontend Task</h3>
+      <img src={ing} alt="" />
+    </div>
     </form>
   </>;
 };
